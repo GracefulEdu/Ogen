8d5269d221df57964b2cbe10e81d10af365f85bf
GracefulEdu <gracefuleducation77@gmail.com>
2025-11-18 22:48:09 +0000

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyDTOPu0jR0UPtlDo53Kk9bkrzZG_on7V3c",
+    authDomain: "ticketsformgis.firebaseapp.com",
+    projectId: "ticketsformgis",
+    storageBucket: "ticketsformgis.firebasestorage.app",
+    messagingSenderId: "430592761100",
+    appId: "1:430592761100:web:6c496b5a4438d400c53d29"
+};
+
